5cd05cb5d97f073859f3adb5adef6e46658cc1d8
Keanelek Enns <keanelekenns@gmail.com>
2022-04-07 00:30:19 -0700

    split file into lines + remove spaces

--- parseFile.js
+++ parseFile.js
@@ -2,17 +2,19 @@
  * @file Module for parsing a text file according to requirements.txt
  * @author Keanelek Enns 
  */
-import {readFile} from 'fs/promises';
+import { readFile } from 'fs/promises';
 
 /**
  * 
  * @param {string} filePath The relative path to the file to parse.
  * @returns {Object} The object representation of the file according to requirements.txt.
  */
-export default async function parseFile(filePath) {
+export default async function parseFile (filePath) {
     try {
-        const file = await readFile(filePath, 'utf8');
-        console.log(file);
+        const fileContents = await readFile(filePath, 'utf8');
+        const lines = fileContents.split(/\r?\n/);
+        const linesNoSpaces = lines.map((line) => line.replace(/\s+/g, ''));
+        console.log(linesNoSpaces);
     } catch (err) {
         console.error(err);
     }
